4ca3977ecc56f83352eae357129b33afdc2ae40c
Ajeey7 <alihas937@hotmail.com>
2025-10-26 02:29:42 +0200

    App.tsx uppdaterad för att exponera registreringssidan via /register.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import PostsPage from './pages/PostsPage';
+import PostForm from './pages/PostForm';
+import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute';
+import Register from './pages/Register';
 
 export default function App() {
   return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Min Anslagstavla</h1>
-      <nav style={{ marginBottom: '1rem' }}>
-        <Link to="/posts" style={{ marginRight: '1rem' }}>Inlägg</Link>
-        <Link to="/posts/new">Nytt Inlägg</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/posts" element={<div>Här är alla inlägg</div>} />
-        <Route path="/posts/new" element={<div>Lägg till nytt inlägg</div>} />
-        <Route path="*" element={<div>Välkommen! Välj en länk ovan.</div>} />
-      </Routes>
-    </div>
+    <>
+      <NavBar />
+      <div style={{ marginTop: '80px' }}>
+        <Routes>
+          <Route path="/" element={<PostsPage />} />
+          <Route path="/posts" element={<PostsPage />} />
+          <Route path="/posts/new" element={<ProtectedRoute><PostForm /></ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </>
   );
 }
